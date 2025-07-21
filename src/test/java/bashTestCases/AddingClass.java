d705fb9c3e85cb8a9ce6d0c1fe13635384de8abe
fatima-nargis <fatima25.nargis@gmail.com>
2025-07-21 18:13:04 +0530

    added

--- src/test/java/bashTestCases/AddingClass.java
+++ src/test/java/bashTestCases/AddingClass.java
@@ -5,6 +5,7 @@ public class AddingClass {
 	public static void main(String[] args) {
 		System.out.println("New Class added");
 		System.out.println("changed");
+		System.out.println("changed1");
 	}
 
 }
